7d00add07ea4b5a8324c330bdc6ee72a6e8d0510
Alex Biet <alexandrubiet@gmail.com>
2022-05-26 21:41:50 +0200

    Web3 modal

--- main.js
+++ main.js
@@ -7,3 +7,142 @@ Moralis.start({ serverUrl, appId });
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 const ethers = Moralis.web3Library;
 let provider;
+
+
+
+////////////////
+///Web3 Modal///
+////////////////
+const Web3Modal = window.Web3Modal.default;
+const WalletConnectProvider = window.WalletConnectProvider.default;
+const evmChains = window.evmChains;
+
+let web3Modal;
+let selectedAccount;
+
+
+function init() {
+
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        infuraId: "4a50be229d4d485cb7b65eec5e5d9440",
+      }
+    }
+  };
+
+  web3Modal = new Web3Modal({
+    cacheProvider: true,
+    providerOptions,
+    disableInjectedProvider: false,
+  });
+}
+
+
+async function fetchAccountData() {
+
+  //const web3 = new Web3(provider);
+  const chainId = await web3.eth.getChainId();
+  const chainData = evmChains.getChain(chainId);
+  let network = chainData["network"];
+  //console.log("Web3 instance is", web3, "Network is: " + network);
+  document.querySelector("#network-name").textContent = chainData.name;
+
+
+
+  const accounts = await web3.eth.getAccounts();
+  selectedAccount = accounts[0];
+  const selectedAccountBalance = await web3.eth.getBalance(accounts[0]);
+  const selectedEthBalance = web3.utils.fromWei(selectedAccountBalance, "ether");
+  const humanFriendlyBalance = parseFloat(selectedEthBalance).toFixed(4);
+  let selectedBalanceSymbol = "ETH";
+
+  if(network === "rinkeby"){
+    selectedBalanceSymbol = "ETH";
+  }else {
+    selectedBalanceSymbol = chainData["nativeCurrency"].symbol;
+  }
+
+  document.querySelector("#selected-account").textContent = selectedAccount.substring(0,6) + "..." + selectedAccount.slice(-4);
+  document.querySelector("#selected-account-balance").textContent = humanFriendlyBalance + " " + selectedBalanceSymbol;
+
+  // Display fully loaded UI for wallet data
+  document.querySelector("#not-connected").style.display = "none";
+  document.querySelector("#connected").style.display = "inline-block";
+
+}
+
+async function refreshAccountData() {
+  document.querySelector("#connected").style.display = "none";
+  document.querySelector("#not-connected").style.display = "block";
+
+
+  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
+  document.querySelector("#btn-connect").removeAttribute("disabled")
+  await fetchAccountData(provider);
+}
+
+
+async function onConnect() {
+  try {
+    provider = await web3Modal.connect();
+
+    let moralisProvider = provider.isMetaMask;
+
+    if (moralisProvider === undefined) {
+      moralisProvider = "walletconnect";
+    } else {
+      moralisProvider = "metamask";
+    }
+    await Moralis.enableWeb3({ provider: moralisProvider });
+
+  } catch(e) {
+    console.log("Could not get a wallet connection", e);
+    return;
+  }
+
+  // Subscribe to accounts change
+  provider.on("accountsChanged", (accounts) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to chainId change
+  provider.on("chainChanged", (chainId) => {
+    fetchAccountData();
+  });
+
+  // Subscribe to networkId change
+  provider.on("networkChanged", (networkId) => {
+    fetchAccountData();
+  });
+  await refreshAccountData();
+}
+
+
+
+async function onDisconnect() {
+
+  console.log("Killing the wallet connection", provider);
+
+  if(provider.close) {
+    await provider.close();
+    await web3Modal.clearCachedProvider();
+    provider = null;
+  }
+  localStorage.removeItem("WEB3_CONNECT_CACHED_PROVIDER");
+  await web3Modal.clearCachedProvider();
+  selectedAccount = null;
+
+  document.querySelector("#not-connected").style.display = "block";
+  document.querySelector("#connected").style.display = "none";
+}
+
+
+window.addEventListener('load', async () => {
+  init();
+  if(localStorage.getItem("WEB3_CONNECT_CACHED_PROVIDER")) await onConnect();
+  document.querySelector("#btn-connect").addEventListener("click", onConnect);
+  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
+
+});
