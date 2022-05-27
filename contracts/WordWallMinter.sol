42c8bafa9b9674bb6ce8d817d0f7166bb9ee5dac
Markeljan <Markeljan19@gmail.com>
2022-05-27 16:04:38 -0400

    minter update

--- contracts/WordWallMinter.sol
+++ contracts/WordWallMinter.sol
@@ -34,9 +34,9 @@ contract WordWallMinter is ERC721,ERC721URIStorage, Ownable {
     }
 
 
-    function updateURI(uint256 _randomVals, uint256 _requestId) external onlyVRFContract {
+    function updateURI(uint256 _randomNum, uint256 _requestId) external onlyVRFContract {
          string memory newUriPath;
-         newUriPath = Strings.toString(_randomVals);
+         newUriPath = Strings.toString(_randomNum);
          newUriPath = string.concat( "uriMainDirectory" , newUriPath);
          _setTokenURI(requestIdToTokenId[_requestId], newUriPath);
 
